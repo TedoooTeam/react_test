c810333e86a47c140c39a0cfe2c153dbc9556385
morapelker <morapelker@gmail.com>
2023-04-02 17:56:11 +0300

    added images

---
+++ src/emojiis.ts
@@ -0,0 +1,10 @@
+export const EMOJI_TABLE = {
+    "heart": 'jpeg',
+    "happy": 'png',
+    "thinking": 1,
+}
+
+export const EMOJI_WELCOME_MESSAGE = (email: string): string =>
+    `Welcome to the tedooo ${email}! :happy:
+    Hope you are having a great day! :heart::heart:
+    `;
