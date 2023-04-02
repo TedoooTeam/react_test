102e5b5e6a8a611d9dde51b966c3625137411bc6
morapelker <morapelker@gmail.com>
2023-04-02 18:07:07 +0300

    more steps

---
+++ src/emojiisv4.ts
@@ -0,0 +1,11 @@
+export const EMOJI_TABLE = {
+    "heart": "jpeg",
+    "happy": "png",
+    "thinking": "png",
+}
+
+export const EMOJI_WELCOME_MESSAGE = (email: string): string =>
+    `Welcome to Tedooo ${email}! Happy
+    Hope you are having a great day! heaRtHEART
+    For more information, visit: www.tedooo.com thinking
+    `;
